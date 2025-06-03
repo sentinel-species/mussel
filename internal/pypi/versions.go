1923a39150a1afeeb25ecf8ddff0801da0eb703c
Anders <1762048+ahokinson@users.noreply.github.com>
2025-06-03 11:02:12 -0400

    Dependency Trees (#3)

--- internal/pypi/versions.go
+++ internal/pypi/versions.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"mussel/internal/config"
 	"os/exec"
-	"path/filepath"
 	"strings"
 
 	"mussel/internal/types"
@@ -28,10 +27,10 @@ func CheckDependencies(pkg string, version string) (*types.DependencyTree, error
 }
 
 func checkDependencies(pkg string) (*types.DependencyTree, error) {
-	cmd := exec.Command(filepath.Join(config.Config.Pypi.PathVenv, "bin", "python"), "-m", "pip", "show", pkg)
+	cmd := exec.Command(config.Config.Pypi.GetPythonPath(), "-m", "pip", "show", pkg)
 	showOutput, err := cmd.CombinedOutput()
 	if err != nil {
-		return nil, fmt.Errorf("failed to get packagezs info for %s: %w (output: %s)", pkg, err, string(showOutput))
+		return nil, fmt.Errorf("failed to get package info for %s: %w (output: %s)", pkg, err, string(showOutput))
 	}
 
 	tree := &types.DependencyTree{
@@ -43,11 +42,14 @@ func checkDependencies(pkg string) (*types.DependencyTree, error) {
 			tree.Version = strings.TrimSpace(strings.TrimPrefix(line, "Version:"))
 		}
 		if strings.HasPrefix(line, "Requires:") {
-			dependencies := strings.Split(strings.TrimSpace(strings.TrimPrefix(line, "Requires:")), ",")
+			requires := strings.TrimPrefix(line, "Requires: ")
+			if requires == "" {
+				continue
+			}
+			dependencies := strings.Split(requires, ", ")
 
 			tree.Dependencies = []*types.DependencyTree{}
 			for _, dependency := range dependencies {
-
 				dependencyTree, err := checkDependencies(dependency)
 				if err != nil {
 					return nil, fmt.Errorf("error processing dependency %s: %w", dependency, err)
